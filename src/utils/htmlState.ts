12fd96f58cc45e4078b8a1529dd5993fbd22a28c
Никита Гура <nikita@MacBook-Air-Nikita.local>
2025-04-17 09:45:44 +0300

    fix: modal

--- src/utils/htmlState.ts
+++ src/utils/htmlState.ts
@@ -1,7 +1,15 @@
 export const lockHTMLElement = (): void => {
-    document.documentElement.classList.add('is-lock')
+    const scrollY = window.scrollY;
+    document.documentElement.classList.add('is-lock');
+    document.body.classList.add('is-lock');
+    document.body.style.top = `-${scrollY}px`;
+    document.body.dataset.scrollY = `${scrollY}`;
 }
 
 export const unlockHTMLElement = (): void => {
-    document.documentElement.classList.remove('is-lock')
+    const scrollY = document.body.dataset.scrollY;
+    document.documentElement.classList.remove('is-lock');
+    document.body.classList.remove('is-lock');
+    document.body.style.top = '';
+    window.scrollTo(0, parseInt(scrollY || '0'));
 }
